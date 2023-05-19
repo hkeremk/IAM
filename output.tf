541ae84024fd7d5a85f03030bc75eb218b5d2f74
hkeremk <74671922+hkeremk@users.noreply.github.com>
2023-05-19 18:30:32 +0300

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,18 @@
+output "user1" {
+  value = aws_iam_user.user1.arn
+}
+output "user2" {
+  value = aws_iam_user.user2.arn
+}
+output "user3" {
+  value = aws_iam_user.user3.arn
+}
+output "user4" {
+  value = aws_iam_user.user4.arn
+}
+output "group1" {
+  value = aws_iam_group.group1.arn
+}
+output "group2" {
+  value = aws_iam_group.group2.arn
+}
